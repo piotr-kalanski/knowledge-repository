298b26eb25dbf45ed2ee33b88275909fb2490e05
piotr-kalanski <piotr.kalanski@gmail.com>
2019-10-31 17:16:21 +0100

    react unit test with enzyme

--- react-unit-test/src/components/Card.test.js
+++ react-unit-test/src/components/Card.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Card from './Card';
-
+import { shallow } from 'enzyme';
 
 it('test', () => {
-  
+    const wrapper = shallow(<Card />);
+    
+    expect(wrapper.text()).to.equal('Card');
 });
